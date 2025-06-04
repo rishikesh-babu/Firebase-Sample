03f17e930fd0468ffae0efa622b82a30979894e8
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-06-04 20:59:04 +0530

    Change background image path

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Login from './Pages/Login'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/routes'
 import { ToastContainer } from 'react-toastify'
+import bgImage from '/backgroundImage.jpeg';
 
 function App() {
 
     return (
-        <div className="min-h-[100dvh] bg-[url('./backgroundImage.jpeg')] bg-cover bg-no-repeat bg-top">
+        <div style={{ backgroundImage: `url(${bgImage})` }} className="min-h-[100dvh] bg-cover bg-no-repeat bg-top">
             <RouterProvider router={router} />
             <ToastContainer theme='dark' position='top-center' />
         </div>
